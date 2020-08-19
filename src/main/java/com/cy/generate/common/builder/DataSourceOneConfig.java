e9d7a45355799600b1b7dad0457ce784873322cd
yongjingchuan <Pwd@19930818>
2020-08-19 22:11:02 +0800

    modified

--- src/main/java/com/cy/generate/common/builder/DataSourceOneConfig.java
+++ src/main/java/com/cy/generate/common/builder/DataSourceOneConfig.java
@@ -1,12 +1,35 @@
 package com.cy.generate.common.builder;
 
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.logging.stdout.StdOutImpl;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
 /**
  * @Description:
  * @Author: YongJingChuan
  * @Date: 2020/8/19 19:46
  */
 //@Configuration
-//@MapperScan(basePackages = {"xxx.xxx.xxx", "xxx.xxx.xxx"}, sqlSessionTemplateRef = "sqlSessionTemplate1")
+//@MapperScan(basePackages = {"xxx.xxx.a.mapper", "xxx.xxx.mapper"}, sqlSessionTemplateRef = "sqlSessionTemplate1")
 public class DataSourceOneConfig {
 
 //    @Bean(name = "dbProperties1")
@@ -38,11 +61,44 @@ public class DataSourceOneConfig {
 //        return dataSource;
 //    }
 //
+//    @Bean(name = "globalConfig")
+//    @Primary
+//    public GlobalConfig globalConfig() {
+//        GlobalConfig globalConfig = new GlobalConfig();
+//        return globalConfig;
+//    }
+//
 //    @Bean(name = "sessionFactory1")
 //    @Primary
-//    public SqlSessionFactory sqlSessionFactory(@Qualifier(value = "datasource1") DataSource dataSource) throws Exception {
+//    public SqlSessionFactory sqlSessionFactory(@Qualifier(value = "datasource1") DataSource dataSource, GlobalConfig globalConfig) throws Exception {
 //        MybatisSqlSessionFactoryBean bean = new MybatisSqlSessionFactoryBean();
 //        bean.setDataSource(dataSource);
+//        bean.setGlobalConfig(globalConfig());
+//
+//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        // 需要在这里指定xml文件的位置，不然自定义的sql会报Invalid bound statement异常
+//        List<String> mapperLocations = new ArrayList<>();
+//        mapperLocations.add("classpath:/biz/mybatis/mapper/*Mapper.xml");
+//        mapperLocations.add("classpath:/mybatis/mapper/*Mapper.xml");
+//        List<Resource> resources = new ArrayList();
+//        if (mapperLocations != null) {
+//            for (String mapperLocation : mapperLocations) {
+//                try {
+//                    Resource[] mappers = resolver.getResources(mapperLocation);
+//                    resources.addAll(Arrays.asList(mappers));
+//                } catch (IOException e) {
+//                    // ignore
+//                }
+//            }
+//        }
+//        bean.setMapperLocations(resources.toArray(new Resource[resources.size()]));
+//
+//        // 导入mybatis配置
+//        MybatisConfiguration mybatisConfiguration = new MybatisConfiguration();
+//        // 配置打印sql语句
+//        mybatisConfiguration.setLogImpl(StdOutImpl.class);
+//        bean.setConfiguration(mybatisConfiguration);
+//
 //        return bean.getObject();
 //    }
 //
